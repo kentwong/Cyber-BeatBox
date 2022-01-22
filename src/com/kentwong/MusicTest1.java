7e43a131aa9b4fb7ad0eafc8164522699c17187f
Kent Wong <kent_wong@outlook.com>
2022-01-22 17:44:35 +1100

    Mini Music App

--- src/com/kentwong/MusicTest1.java
+++ src/com/kentwong/MusicTest1.java
@@ -1,10 +1,24 @@
 package com.kentwong;
 
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequencer;
+
 public class MusicTest1 {
 
+	public void play() {
+		try {
+			Sequencer sequencer = MidiSystem.getSequencer();
+		} catch (MidiUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("We got a sequencer");
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("HelloWorld");
+		MusicTest1 mt = new MusicTest1();
+		mt.play();
 	}
 
 }
